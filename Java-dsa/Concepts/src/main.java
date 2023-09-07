942725fc4a5d521aa75297e3e8e9846b4fa7f2da
anshumancodes <anshumanprof01@gmail.com>
2023-09-07 14:08:57 +0530

    intial commit

---
+++ Java-dsa/Concepts/src/main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+      System.out.println("please enter a number ");
+        Scanner input = new Scanner(System.in);
+        System.out.println("user entered " +input.nextInt());
+    }
+}
