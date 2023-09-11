c24cbb2e7a6e3edfa5ea0e862d9c61910f113a29
anshumancodes <anshumanprof01@gmail.com>
2023-09-12 00:05:31 +0530

    switch and conditional question

---
+++ Java-dsa/Concepts/src/noofdaysallowed.java
@@ -0,0 +1,32 @@
+public class noofdaysallowed {
+    public static void main(String[] args) {
+//        //Kunal is allowed to go out with his friends only on the even days of a given month.
+//        // Write a program to count the number of days he can go out in the month of August.
+//
+//        int days=31;
+//        int i;
+//        int count=0;
+//
+//        for (i=1;i<=days;i++){
+//            if (i%2==0){
+//                count++;
+//            }
+//        }
+//
+//        System.out.println("number of days kunal can go out in august is "+count);
+
+        //or
+//        for (int i = 1; i <= days; i++) {
+//            switch (i % 2) {
+//                case 0:
+//                    count++;
+//                    break;
+//            }
+//        }
+//        System.out.println("number of days kunal can go out in august is " + count);
+
+
+
+
+    }
+}
---
+++ Java-dsa/Concepts/src/switchcaseandnestedcase.java
@@ -0,0 +1,2 @@
+public class switchcaseandnestedcase {
+}
