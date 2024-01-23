6c57491bee3e9932d0118c65346f6ad70ca7793d
anshumancodes <anshumanprof01@gmail.com>
2024-01-23 09:09:20 +0530

    Remove Java-dsa directory old dir

--- Java-dsa/Concepts/src/main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class main {
-    public static void main(String[] args) {
-      System.out.println("please enter a number ");
-        Scanner input = new Scanner(System.in);
-        System.out.println("user entered " +input.nextInt());
-    }
-}
--- Java-dsa/Concepts/src/switchcaseandnestedcase.java
+++
@@ -1,5 +0,0 @@
-public class switchcaseandnestedcase {
-    public static void main(String[] args) {
-        
-    }
-}
